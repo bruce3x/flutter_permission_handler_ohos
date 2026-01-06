1a0e31aa7593a2dd964bc3ca8697137aad679098
wuleilei <wuleilei3@h-partners.com>
2026-01-06 10:06:12 +0800

    feat:升级permission_handler到12.0.1版本，支持flutter ohos 3.35.7版本
    Signed-off-by: wuleilei <wuleilei3@h-partners.com>

--- README.OpenHarmony.md
+++ README.OpenHarmony.md
@@ -24,7 +24,7 @@ dependencies:
     git: 
       url: https://gitcode.com/openharmony-sig/flutter_permission_handler.git
       path: permission_handler_ohos
-      ref: br_permission_handler_v11.3.1_ohos
+      ref: br_permission_handler_v12.0.1_ohos
       
 ...
 ```
@@ -49,6 +49,7 @@ flutter pub get
 This document is verified based on the following versions:
 
 1. Flutter: 3.22.1-ohos-1.0.1; SDK: 5.0.0(12); IDE: DevEco Studio: 5.0.13.200; ROM: 5.1.0.120 SP3;
+2.Flutter: 3.35.7-ohos-0.0.1; SDK: 6.0.1(21); IDE: DevEco Studio: 6.0.1.260; ROM: 6.0.0.120 SP6;
 
 
 ## 3. API
--- permission_handler_apple/ios/Classes/strategies/LocationPermissionStrategy.m
+++ permission_handler_apple/ios/Classes/strategies/LocationPermissionStrategy.m
@@ -36,7 +36,13 @@ NSString *const UserDefaultPermissionRequestedKey = @"org.baseflow.permission_ha
 }
 
 - (void)checkServiceStatus:(PermissionGroup)permission completionHandler:(ServiceStatusHandler)completionHandler {
-    completionHandler([CLLocationManager locationServicesEnabled] ? ServiceStatusEnabled : ServiceStatusDisabled);
+    dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        BOOL isEnabled = [CLLocationManager locationServicesEnabled];
+
+        dispatch_async(dispatch_get_main_queue(), ^(void) {
+            completionHandler(isEnabled ? ServiceStatusEnabled : ServiceStatusDisabled);
+        });
+    });
 }
 
 - (void)requestPermission:(PermissionGroup)permission completionHandler:(PermissionStatusHandler)completionHandler errorHandler:(PermissionErrorHandler)errorHandler {
@@ -168,37 +174,20 @@ NSString *const UserDefaultPermissionRequestedKey = @"org.baseflow.permission_ha
 
 
 + (PermissionStatus)determinePermissionStatus:(PermissionGroup)permission authorizationStatus:(CLAuthorizationStatus)authorizationStatus {
-    if (@available(iOS 8.0, *)) {
-        if (permission == PermissionGroupLocationAlways) {
-            switch (authorizationStatus) {
-                case kCLAuthorizationStatusNotDetermined:
-                    return PermissionStatusDenied;
-                case kCLAuthorizationStatusRestricted:
-                    return PermissionStatusRestricted;
-                case kCLAuthorizationStatusAuthorizedWhenInUse:
-                case kCLAuthorizationStatusDenied:
-                    return PermissionStatusPermanentlyDenied;
-                case kCLAuthorizationStatusAuthorizedAlways:
-                    return PermissionStatusGranted;
-            }
-        }
-        
+    if (permission == PermissionGroupLocationAlways) {
         switch (authorizationStatus) {
             case kCLAuthorizationStatusNotDetermined:
                 return PermissionStatusDenied;
             case kCLAuthorizationStatusRestricted:
                 return PermissionStatusRestricted;
+            case kCLAuthorizationStatusAuthorizedWhenInUse:
             case kCLAuthorizationStatusDenied:
                 return PermissionStatusPermanentlyDenied;
-            case kCLAuthorizationStatusAuthorizedWhenInUse:
             case kCLAuthorizationStatusAuthorizedAlways:
                 return PermissionStatusGranted;
         }
     }
     
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-
     switch (authorizationStatus) {
         case kCLAuthorizationStatusNotDetermined:
             return PermissionStatusDenied;
@@ -206,6 +195,15 @@ NSString *const UserDefaultPermissionRequestedKey = @"org.baseflow.permission_ha
             return PermissionStatusRestricted;
         case kCLAuthorizationStatusDenied:
             return PermissionStatusPermanentlyDenied;
+        case kCLAuthorizationStatusAuthorizedWhenInUse:
+        case kCLAuthorizationStatusAuthorizedAlways:
+            return PermissionStatusGranted;
+    }
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+
+    switch (authorizationStatus) {
         case kCLAuthorizationStatusAuthorized:
             return PermissionStatusGranted;
         default:
